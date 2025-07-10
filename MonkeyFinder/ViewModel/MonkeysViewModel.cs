6eba5da0c8bae05eb0a203959c370daef279e28d
brcrodrigues96@gmail.com <brcrodrigues96@gmail.com>
2025-07-10 15:00:23 +0100

    Implement geolocation and map integration for finding closest monkey

--- MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -9,12 +9,56 @@ public partial class MonkeysViewModel : BaseViewModel
 
     public ObservableCollection<Monkey> Monkeys { get; } = new();
 
+    IConnectivity connectivity;
+    IGeolocation geolocation;
 
-
-    public MonkeysViewModel(MonkeyService monkeyService)
+    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
     {
         Title = "Monkey Finder";
         this.monkeyService = monkeyService;
+        this.connectivity = connectivity;
+        this.geolocation = geolocation;
+    }
+
+
+    [RelayCommand]
+    async Task GetClosestMonkeyAsync()
+    {
+        if (IsBusy || Monkeys.Count == 0)
+            return;
+
+            try
+            {
+                var location = await geolocation.GetLastKnownLocationAsync();
+                if (location == null)
+                {
+                    location = await geolocation.GetLocationAsync(
+                        new GeolocationRequest
+                        {
+                            DesiredAccuracy = GeolocationAccuracy.Medium,
+                            Timeout = TimeSpan.FromSeconds(30),
+
+
+                        });
+                }
+
+                if (location is null)
+                    return;
+
+                var first = Monkeys.OrderBy(m =>
+                location.CalculateDistance(m.Latitude, m.Longitude, DistanceUnits.Miles)).FirstOrDefault();
+
+                if (first is null)
+                    return;
+
+                await Shell.Current.DisplayAlert("Closest Monkey", $"{first.Name} in {first.Location}", "OK");
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await Shell.Current.DisplayAlert("Error!", $"Unable to get closest monkey: {ex.Message}", "OK");
+            }
     }
 
     [RelayCommand]
@@ -39,6 +83,14 @@ public partial class MonkeysViewModel : BaseViewModel
 
         try
         {
+
+            if (connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert("Internet issue", $"Check your internet and try again!", "OK");
+                return;
+            }
+
+
             IsBusy = true;
             var monkeys = await monkeyService.GetMonkeys();
 
